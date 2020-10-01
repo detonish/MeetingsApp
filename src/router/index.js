0c14644190ed383a068f31efaaab8b09d0f09137
detonish <aragonpalma.nico@gmail.com>
2020-10-01 16:06:43 -0300

    App ready

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,49 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Router from "vue-router";
+import Home from "@/views/Home.vue";
+import Login from "@/views/Login.vue";
+import Meetings from "@/views/Meetings.vue";
+import Register from "@/views/Register.vue";
+import CheckIn from "@/views/CheckIn.vue";
+import Attendees from "@/views/Attendees";
 
-Vue.use(VueRouter);
+Vue.use(Router);
 
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
-
-const router = new VueRouter({
-  routes
+export default new Router({
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
+    },
+    {
+      path: "/meetings",
+      name: "meetings",
+      component: Meetings
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register
+    },
+    {
+      path: "/checkin/:userID/:meetingID",
+      name: "CheckIn",
+      component: CheckIn
+    },
+    {
+      path: "/attendees/:userID/:meetingID",
+      name: "Attendees",
+      component: Attendees
+    },
+    {
+      path: "*",
+      redirect: "/"
+    }
+  ]
 });
-
-export default router;
